edbc499f631d2c04d61409028367ece5def457cf
cr0w1 <clebsonjose30@gmail.com>
2020-02-13 21:36:22 -0300

    Verificações e fronte aprimorados!!

--- src/main/resources/static/assets/js/validacao.js
+++ src/main/resources/static/assets/js/validacao.js
@@ -1,33 +1,5 @@
 $(document).ready(function () { 
 
-    jQuery.validator.addMethod("cpf", function(value, element) {
-        value = jQuery.trim(value);
-
-        value = value.replace('.','');
-        value = value.replace('.','');
-        cpf = value.replace('-','');
-        while(cpf.length < 11) cpf = "0"+ cpf;
-        var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
-        var a = [];
-        var b = new Number;
-        var c = 11;
-        for (i=0; i<11; i++){
-            a[i] = cpf.charAt(i);
-            if (i < 9) b += (a[i] * --c);
-        }
-        if ((x = b % 11) < 2) { a[9] = 0 } else { a[9] = 11-x }
-        b = 0;
-        c = 11;
-        for (y=0; y<10; y++) b += (a[y] * c--);
-        if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11-x; }
-
-        var retorno = true;
-        if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) retorno = false;
-
-        return this.optional(element) || retorno;
-
-    }, "Informe um CPF válido");
-
     //FORM REGISTRO                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
     $("#formCheck").validate({
 	    rules:{
@@ -57,18 +29,75 @@ $(document).ready(function () {
 	    },
 	    messages:{
 	    	nome:{
-	    		require: "O campo é obrigatório."
+	    		require: "<font color='red'>O campo é obrigatório.</font>"
 	    	},
 	        cpf:{
-	            required: "O campo cpf é obrigatório.",
-	            remote: "Acesso negado!",
-	            checkFlag: "Bem vindo e aproveite o dia",
-	            cpf: "CPF invalido",
+	            required: "<font color='red'>O campo cpf é obrigatório.</font>",
+	            remote: "<font color='red'>Acesso negado!</font>",
+	            checkFlag: "<font color='red'>Bem vindo e aproveite o dia</font>",
+	            cpf: "<font color='red'>CPF invalido</font>",
 	            
 	        }	
-	    }
+	    },
+	    errorElement: 'span',
+	    errorPlacement: function(error, element) {
+	        if(element.attr('name') == 'cpf'){
+	        	
+	        	if(error[0].innerText != "CPF invalido"){
+	        		$(".cpf").prepend(error);
+	        	}else{
+	        		$(".cpf").prepend(error);
+	        	}
+	        	
+	         }
+	         if(element.attr('name') == 'nome'){
+	        	 $(".nome").prepend(error[0]);
+	         }
+	    },
+	    success: function(label) {
+            var parentId = " " + label[0].outerHTML;
+            var separa = parentId.split('"');
+
+            var valor = separa[1];
+            
+            switch(valor) {
+                case "cpf-error":
+                	$("#1nome").attr("hidden",false);
+                	console.log('foi')
+                    break;
+            }                    
+        }
             
     });
+    
+    
+    jQuery.validator.addMethod("cpf", function(value, element) {
+        value = jQuery.trim(value);
+
+        value = value.replace('.','');
+        value = value.replace('.','');
+        cpf = value.replace('-','');
+        while(cpf.length < 11) cpf = "0"+ cpf;
+        var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+        var a = [];
+        var b = new Number;
+        var c = 11;
+        for (i=0; i<11; i++){
+            a[i] = cpf.charAt(i);
+            if (i < 9) b += (a[i] * --c);
+        }
+        if ((x = b % 11) < 2) { a[9] = 0 } else { a[9] = 11-x }
+        b = 0;
+        c = 11;
+        for (y=0; y<10; y++) b += (a[y] * c--);
+        if ((x = b % 11) < 2) { a[10] = 0; } else { a[10] = 11-x; }
+
+        var retorno = true;
+        if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) retorno = false;
+
+        return this.optional(element) || retorno;
+
+    }, "Informe um CPF válido");
    
     jQuery.validator.addMethod("checkFlag", function(value, element) {
 		   value = jQuery.trim(value);
